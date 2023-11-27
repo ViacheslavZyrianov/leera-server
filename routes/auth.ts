17a823028b57e0f21075bbb3e5b7742a44110fa8
ViacheslavZyrianov <viacheslavz.dev@support.team>
2023-11-27 21:28:05 +0200

    remove comments

--- routes/auth.ts
+++ routes/auth.ts
@@ -3,20 +3,11 @@ const express = require('express')
 const {
     postUserRegister,
     postUserLogin,
-    // getStoryById,
-    // patchStory,
-    // deleteStory
 } = require('../controllers/user')
 
 const router = express.Router()
 
 router.post('/register', postUserRegister)
 router.post('/login', postUserLogin)
-
-// router.get('/:id', getStoryById)
-
-// router.patch('/:id', patchStory)
-
-// router.delete('/:id', deleteStory)
 
 module.exports = router
